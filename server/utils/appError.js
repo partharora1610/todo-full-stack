a852d49f6120444a3f1270216b553f9112168196
partharora1610 <partharora16102001@gmail.com>
2023-10-03 12:40:55 +0530

    added AppError class

---
+++ server/utils/appError.js
@@ -0,0 +1,15 @@
+// These will be operational error
+class AppError extends Error {
+  constructor(message, statusCode) {
+    // calling the parent class Error
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
